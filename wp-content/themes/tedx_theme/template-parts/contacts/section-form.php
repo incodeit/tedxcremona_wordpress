bf5a7168289966e57a0b9ce3f1937349c2377797
vBalatroni <valerio.balatroni@gmail.com>
2021-01-07 20:19:42 +0100

    contact form wip + database

--- wp-content/themes/tedx_theme/template-parts/contacts/section-form.php
+++ wp-content/themes/tedx_theme/template-parts/contacts/section-form.php
@@ -1,1 +1,8 @@
-  <?php include(locate_template('template-parts/section-hero.php')); ?>
+<?php $form = get_sub_field('form'); ?>
+
+<section class="section--contact">
+    <div class="section__contact-form">
+        <?php echo $form; ?>
+    </div>
+</section>
+
